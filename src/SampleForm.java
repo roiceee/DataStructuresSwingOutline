6fda753d980b1ba49ccee800f0a9747bf7ce81c1
jroiceee <jroicealdeza@gmail.com>
2022-12-23 21:02:12 +0800

    add getInput and showOutput methods

--- src/SampleForm.java
+++ src/SampleForm.java
@@ -1,5 +1,5 @@
 import javax.swing.*;
-import java.awt.*;
+import java.util.ArrayList;
 
 public class SampleForm {
     private JPanel mainForm;
@@ -8,16 +8,21 @@ public class SampleForm {
     private JLabel outputLabel;
     private JButton addButton;
     private JButton deleteButton;
-    private JButton action3Button;
+    private JButton getButton;
     private JTextArea output;
-    private JButton action4Button;
-    private JButton action5Button;
+    private JButton containsButton;
+    private JButton sizeButton;
     private JLabel title;
     private JPanel actionDiv;
     private JPanel outputDiv;
     private JPanel titleDiv;
 
+    //add data structure here
+    //ArrayList<String> list = new ArrayList<>();
+
+
     public SampleForm() {
+
     }
 
     public static void main(String[] args) {
@@ -29,6 +34,15 @@ public class SampleForm {
         frame.setVisible(true);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
+
+    }
+
+    public String getInput(String message) {
+        return JOptionPane.showInputDialog(message);
+    }
+
+    public void showOutput(Object object) {
+        output.setText(object.toString());
     }
 
 }
